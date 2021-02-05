e413d8d9d30d6c3151b0c6c80814572771047cbf
callynz <cmz0007@mix.wvu.edu>
2021-02-04 23:14:02 -0500

    Lab2 About

---
+++ index.php
@@ -0,0 +1,58 @@
+<main>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1>About Us</h1>
+        <p style="color: white">Meet the Team!</p>
+    </div>
+
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/image1.jpeg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p style="color: red">Red</p>
+                    <p>Email: red@mandm.com</p>
+                    <p style="color: red">Meet Red! Red is the bright CEO of our company. He brings the personality to the table.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="images/image2.jpeg">
+                <div class="container">
+                    <h2>COO</h2>
+                    <p style="color: orange">Yellow</p>
+                    <p>Email: yellow@mandm.com</p>
+                    <p style="color: orange">Meet Yellow! Yellow is our sunshine COO. He is a friendly soul.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="images/image3.jpeg">
+                <div class="container">
+                    <h2>CFO</h2>
+                    <p style="color: blue">Blue</p>
+                    <p>Email: blue@mandm.com</p>
+                    <p style="color: blue">Meet Blue! Although he may look intimidating, Blue is a charismatic genius.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="images/image4.jpeg">
+                <div class="container">
+                    <h2>CMO</h2>
+                    <p style="color: brown">Brown</p>
+                    <p>Email: brown@mandm.com</p>
+                    <p style="color: brown">Meet Brown! She is the brains of our operation. Female power for the win.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
